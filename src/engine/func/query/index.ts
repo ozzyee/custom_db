899fdd365c278e0ed52251416485bb65d10a4dfe
ozzyee <oearle10@gmail.com>
2025-03-23 19:39:36 +0000

    Refactor query execution by replacing Query class instantiation with a query function in index.ts. Update SQL statement to insert a new todo item.

--- src/engine/func/query/index.ts
+++ src/engine/func/query/index.ts
@@ -1,8 +1,23 @@
 import { select } from "./select";
 import { insert } from "./insert";
 import { deleteQuery } from "./delete";
-export class Query {
+
+class Query {
   select = (sqlStatment: string) => select(sqlStatment);
   insert = (sqlStatment: string) => insert(sqlStatment);
   delete = (sqlStatment: string) => deleteQuery(sqlStatment);
 }
+
+export const query = (sqlStatment: string) => {
+  const method = sqlStatment.split(" ")[0];
+
+  // Valid methods
+  const validMethods = ["SELECT", "INSERT", "DELETE"];
+
+  if (!validMethods.includes(method)) {
+    throw new Error("Invalid method!");
+  }
+
+  const query = new Query();
+  query[method.toLowerCase() as keyof Query](sqlStatment);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
+import { query } from "./engine/func/query";
 import { Table } from "./engine/func/table";
-import { Query } from "./engine/func/query";
 
 (async () => {
-  const table = new Table();
-  const query = new Query();
-
-  await query.delete(
-    "DELETE FROM todos WHERE id = 10 and title conteins 'groceries'"
-  );
+  query("INSERT INTO todos (title, completed) VALUES ('Buy groceries', false)");
 })();
